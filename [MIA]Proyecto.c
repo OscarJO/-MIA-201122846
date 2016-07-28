b5acd04a05464c6be3bdeb3287f2dbc0a09e5e58
OscarJO <oscar.jordan.21091@gmail.com>
2016-07-27 20:14:48 -0600

    primer commit

---
+++ [MIA]Proyecto.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Universidad de San Carlos de Guatemala.\n");
+    printf("Facultad de Ingenieria.\n");
+    printf("Escuela de Ciencias y Sistemas.\n");
+    printf("Manejo e Implementacion de Archivos.\n");
+    printf("Segundo semestre de 2016.\n\n\n");
+    printf("-------  PROYECTO  -------d");
+    return 0;
+}
