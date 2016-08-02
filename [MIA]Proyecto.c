9e1e264588692a4fbe8e1b07c2d15cd3a7b9a944
OscarJO <oscar.jordan.21091@gmail.com>
2016-08-01 22:45:24 -0600

    commit

--- [MIA]Proyecto.c
+++ [MIA]Proyecto.c
@@ -8,6 +8,30 @@ int main()
     printf("Escuela de Ciencias y Sistemas.\n");
     printf("Manejo e Implementacion de Archivos.\n");
     printf("Segundo semestre de 2016.\n\n\n");
-    printf("-------  PROYECTO  -------d");
+    printf("-------  PROYECTO  -------");
+    //const char* linea;
+    char linea[256];
+    //char *token;
+    printf("\n\n\n");
+    do
+    {
+        printf("Ingrese el comando: \n");
+        //char linea[256];
+        fgets(linea, 256, stdin);
+        printf("linea: %s", linea);
+        char buffer[1024];
+        int rv = sprintf(buffer, linea);
+        //printf("bufer: %s", buffer);
+        char *token = strtok(buffer, " ");
+        //printf("token: %c", token);
+        //token = strtok(NULL, " ");
+        while(token)
+        {
+            printf("token: %s", &token);
+            token = strtok(NULL, " ");
+        }
+    }
+    while(linea != NULL);
+    printf("Saliendo del sistema!");
     return 0;
 }
